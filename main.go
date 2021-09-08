0e1025f518502847669b812cabd0670dd4c79409
Billy Hadlow <bhadlow@live.co.uk>
2021-09-08 23:30:49 +0100

    basic rest http stuff

--- main.go
+++ main.go
@@ -2,18 +2,38 @@ package main
 
 import (
 	"log"
+	"net/http"
 )
 
+// External
 import (
 	bolt "go.etcd.io/bbolt"
+	"github.com/gorilla/mux"
 )
 
+func all(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "hello world"}`))
+}
+
 func main() {
-	db, err := bolt.Open("main.db", 0600, nil)
+	// db, err := bolt.Open("main.db", 0600, nil)
 
-	if err != nil {
-		log.Fatal(err)
-	}
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	defer db.Close()
+	// defer db.Close()
+
+	r := mux.NewRouter()
+	api := r.PathPrefix("/api").Subrouter()
+	posts := api.PathPrefix("/posts").Subrouter()
+
+    posts.HandleFunc("/", all).Methods(http.MethodGet)
+	posts.HandleFunc("/{id}", get).Methods(http.MethodGet)
+	posts.HandleFunc("/", post).Methods(http.MethodPost)
+	posts.HandleFunc("/{id}", put).Methods(http.MethodPut)
+
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
